761f9837159e6b7acaa771a1fc3eb27e665362cf
omaroof <omaroof@liveperson.com>
2018-01-31 11:42:46 +0100

    added test

---
+++ LP-iOS-3/LPView.h
@@ -0,0 +1,13 @@
+//
+//  LPView.h
+//  LP-iOS-3
+//
+//  Created by Omer Maroof on 31.01.18.
+//  Copyright © 2018 LivePerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPView : UIView
+
+@end
---
+++ LP-iOS-3/LPView.m
@@ -0,0 +1,21 @@
+//
+//  LPView.m
+//  LP-iOS-3
+//
+//  Created by Omer Maroof on 31.01.18.
+//  Copyright © 2018 LivePerson. All rights reserved.
+//
+
+#import "LPView.h"
+
+@implementation LPView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
