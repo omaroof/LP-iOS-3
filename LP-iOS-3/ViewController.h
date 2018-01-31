fd3a4e6502f61d67c2b4887ff6d8c9b06f5a16b4
omaroof <omaroof@liveperson.com>
2018-01-31 11:21:39 +0100

    Initial Commit

---
+++ LP-iOS-3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LP-iOS-3
+//
+//  Created by Omer Maroof on 31.01.18.
+//  Copyright © 2018 LivePerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LP-iOS-3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LP-iOS-3
+//
+//  Created by Omer Maroof on 31.01.18.
+//  Copyright © 2018 LivePerson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
